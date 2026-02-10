8dc1d0569449c30f5b2818cb24a328206240fadd
TharHtet236401 <tharhteta3@gmail.com>
2026-02-10 19:46:44 +0000

    tailwind applied

--- apps/posts/selectors.py
+++ apps/posts/selectors.py
@@ -2,7 +2,7 @@ from .models import Post
 
 def get_posts():
     try:
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-created_at')
         return posts
     except Exception as e:
         print(e)
