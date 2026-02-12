e3281e1abf07c982f27deb6efe84f6a993673e23
TharHtet236401 <tharhteta3@gmail.com>
2026-02-12 18:48:12 +0000

    theme applied

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
-    path('posts/', include('apps.posts.urls')),
+    path('home/', include('apps.core.urls')),
+    path('', include('apps.posts.urls')),
 ]
