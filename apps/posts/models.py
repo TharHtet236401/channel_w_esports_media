655dd245693bdf1c6dcc98f2471fe5d297be77df
TharHtet236401 <tharhteta3@gmail.com>
2026-02-12 19:54:40 +0000

    region added

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     caption = models.CharField(max_length=200)
     description = models.TextField()
+    region = models.ManyToManyField(Region)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
