13b483dfa07901ea0b3fe55de4639d0afff982e9
TharHtet236401 <tharhteta3@gmail.com>
2026-02-15 19:16:43 +0000

    modify the script and models

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from apps.core.constants import Region as RegionEnum
+
 # Create your models here.
 class Region(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, choices=[(r.value, r.value) for r in RegionEnum])
 
     def __str__(self):
         return self.name
