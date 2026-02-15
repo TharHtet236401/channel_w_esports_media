89fee29c8e09c225ad9a0143d7287c8245583339
TharHtet236401 <tharhteta3@gmail.com>
2026-02-15 18:59:00 +0000

    added pagination

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,8 +1,28 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from .models import Post
+
 from .selectors import get_posts
 
-# Create your views here.
+
 def post_list(request):
-    posts = get_posts()
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    try:
+        post_list_qs = get_posts()
+        paginator = Paginator(post_list_qs, 9)
+        page_number = request.GET.get("page", 1)
+        try:
+            page_obj = paginator.page(page_number)
+        except PageNotAnInteger:
+            page_obj = paginator.page(1)
+        except EmptyPage:
+            page_obj = paginator.page(paginator.num_pages)
+        return render(
+            request,
+            "posts/post_list.html",
+            {"posts": page_obj.object_list, "page_obj": page_obj},
+        )
+    except Exception as e:
+        return render(
+            request,
+            "posts/post_list.html",
+            {"posts": [], "page_obj": None, "error": str(e)},
+        )
